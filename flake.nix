49bee06d3ebd1e4bb7a6f5840e746958cf61b493
Ryan <ryanacree13@proton.me>
2026-04-23 22:36:22 -0700

    .

--- flake.nix
+++ flake.nix
@@ -5,11 +5,9 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nix-openclaw.url = "github:openclaw/nix-openclaw";
   };
 
-  # 1. Add nix-openclaw to the outputs arguments
-  outputs = { self, nixpkgs, home-manager, nix-openclaw, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -18,16 +16,13 @@
       nixosConfigurations = {
         craptop = lib.nixosSystem {
           inherit system;
-          # 2. Pass the input into specialArgs so configuration.nix can see it
-          specialArgs = { inherit nix-openclaw; }; 
+ 
           modules = [ ./configuration.nix ];
       };
     };
     homeConfigurations = {
       ryan = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        # 3. Pass the input into extraSpecialArgs for home.nix
-        extraSpecialArgs = { inherit nix-openclaw; };
         modules = [ ./home.nix ];
       };
     };
