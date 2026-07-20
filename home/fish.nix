cf42f99c553126f97aff23db3a1e19e6f456d126
Ryan Acree <ryanacree13@proton.me>
2026-07-19 18:10:03 -0700

    .

--- home/fish.nix
+++ home/fish.nix
@@ -20,6 +20,7 @@ let
     gc   = "git commit -m .";
     hypr = "cd ~/.config/hypr";
     conf = "cd ~/.config";
+    gf   = "ga && gc && gp &&";
     tung = "echo 'tung tung tung sahur'";
   };
 in
