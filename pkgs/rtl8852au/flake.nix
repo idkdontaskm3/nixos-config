6293b1912c4e1805c2a1a6e58a38f011b4ea6ec9
Ryan Acree <ryanacree13@proton.me>
2026-07-27 21:00:24 -0700

    .

---
+++ pkgs/rtl8852au/flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "rtw89 driver";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.default = pkgs.kernelPackages.callPackage ./default.nix {};
+    };
+}
