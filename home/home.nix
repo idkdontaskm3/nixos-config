2e9fde26b0e4c0d704e798123c55d86f696c81dd
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-05 10:54:45 -0700

    .

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,12 @@
     ./fish.nix
   ];
 
+  # hyprland patch, no longer needed when hyprland updates for glaze 8
+
+  nixpkgs.config.packageOverrides = pkgs; {
+    glaze = glaze-stable.legacyPackages.${system}.glaze;
+  }  
+
   home.username = "ryan";
   home.homeDirectory = "/home/ryan";
   home.stateVersion = "26.05";
--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -1,7 +1,5 @@
--- Neovim config - basic QOL setup
 vim.g.mapleader = " "
 
--- Settings
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
@@ -12,68 +10,39 @@ vim.opt.wrap = false
 vim.opt.cursorline = true
 vim.opt.clipboard = "unnamedplus"
 
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
+    "--branch=stable", lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugins
 require("lazy").setup({
-  -- Colorscheme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
     config = function()
-      pcall(function()
-        require("catppuccin").setup({ flavour = "mocha" })
-        vim.cmd.colorscheme("catppuccin")
-      end)
+      require("catppuccin").setup({ flavour = "mocha" })
+      vim.cmd.colorscheme("catppuccin")
     end,
   },
-
-  -- Treesitter (syntax highlighting)
   "nvim-treesitter/nvim-treesitter",
-
-  -- Auto-pairs (auto close brackets, quotes)
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
+  { "windwp/nvim-autopairs", event = "InsertEnter",
+    config = function() require("nvim-autopairs").setup() end,
   },
-
-  -- Mason (install later if needed)
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
-
-  -- Completion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
+  { "hrsh7th/nvim-cmp", dependencies = {
+      "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
     },
     config = function()
       local cmp = require("cmp")
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
+        snippet = { expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -81,28 +50,42 @@ require("lazy").setup({
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = "buffer" },
-          { name = "path" },
+          { name = "buffer" }, { name = "path" },
         }),
       })
-
-      cmp.setup.cmdline(":", {
-        sources = { { name = "cmdline" } },
-      })
     end,
   },
-
-  -- Statusline (optional, nice to have)
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function() require("lualine").setup() end,
+  },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("lualine").setup()
+      local t = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", t.find_files)
+      vim.keymap.set("n", "<leader>fg", t.live_grep)
+      vim.keymap.set("n", "<leader>fb", t.buffers)
     end,
   },
+  { "nvim-neo-tree/neo-tree.nvim", dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    config = function()
+      vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+    end,
+  },
+  { "kdheepak/lazygit.nvim",
+    config = function() vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>") end,
+  },
+  { "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup()
+      vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
+    end,
+  },
+  { "mbbill/undotree",
+    config = function() vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>") end,
+  },
+  { "lewis6991/gitsigns.nvim", config = function() require("gitsigns").setup() end },
 })
 
--- Basic keybinds
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>r", ":!nix-shell --run 'clang % -o out && ./out'<CR>", { noremap = true })
@@ -110,5 +93,5 @@ vim.keymap.set({'n', 'v'}, 'j', 'h', { noremap = true })
 vim.keymap.set({'n', 'v'}, 'k', 'j', { noremap = true })
 vim.keymap.set({'n', 'v'}, 'l', 'k', { noremap = true })
 vim.keymap.set({'n', 'v'}, ';', 'l', { noremap = true })
-vim.keymap.set({'n', 'v'}, 'h',';', {noremap = true})
-vim.keymap.set('i', 'jk', '<Esc>', {noremap = true})
+vim.keymap.set({'n', 'v'}, 'h', ';', { noremap = true })
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true })
