13988a84b6f8ebcfd20df91e461dd4607c8acb0a
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-04 21:07:34 -0700

    .

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,12 +4,10 @@
   services.xserver = {
     enable = true;
     xkb.layout = "us";
-    displayManager.lightdm.enable = false;
     desktopManager.cinnamon.enable = true;
   };
 
   services.displayManager.gdm.enable = true;
-  services.displayManager.sddm.wayland.enable = false;
-  programs.hyprland.enable = true;
+#  programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
