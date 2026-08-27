417afbf2e7846cb2d1b0ef8b7fc748307c6b0eef
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-27 09:37:02 -0700

    .

--- modules/users.nix
+++ modules/users.nix
@@ -17,37 +17,37 @@
     ];
   };
 
-# users.users.tor = {
-#   isSystemUser = false;
-#   description = "User for tor/ .onion related stuff";
-#   home = "/home/home/tor";
-#   createHome = true;
-#   shell = pkgs.nologin;
-#   uid = 1001;
-#   group = "tor";
-#   umask = "0077";
-# };
+ users.users.tor = {
+   isSystemUser = false;
+   description = "User for tor/ .onion related stuff";
+   home = "/home/home/tor";
+   createHome = true;
+   shell = pkgs.nologin;
+   uid = 1001;
+   group = "tor";
+   umask = "0077";
+ };
 
-# users.groups.tor.gid = 1001;
+ users.groups.tor.gid = 1001;
 
-# systemd.tmpfiles.rules = [
-#   "d /tmp/tmp 0700 tor tor - -"
-#   "d /dev/shm/shm 0700 tor tor - -"
-# ];
+ systemd.tmpfiles.rules = [
+   "d /tmp/tmp 0700 tor tor - -"
+   "d /dev/shm/shm 0700 tor tor - -"
+ ];
 
-# environment.etc."firejail/tor.profile".text = ''
-#   whitelist /home/home/tor
-#   whitelist /tmp/tmp
-#   whitelist /dev/shm/shm
-#   whitelist /etc/systemd/resolved.conf
-#   blacklist /home
-#   blacklist /tmp
-#   blacklist /dev/shm
-#   blacklist /root
-#   read-only /
-#   net tor
-#   dns off
-#   dbus-system none
-#   dbus-user none
-# '';
+ environment.etc."firejail/tor.profile".text = ''
+   whitelist /home/home/tor
+   whitelist /tmp/tmp
+   whitelist /dev/shm/shm
+   whitelist /etc/systemd/resolved.conf
+   blacklist /home
+   blacklist /tmp
+   blacklist /dev/shm
+   blacklist /root
+   read-only /
+   net tor
+   dns off
+   dbus-system none
+   dbus-user none
+ '';
 }
