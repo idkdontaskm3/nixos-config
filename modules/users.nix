6fbaf2f2ebb9d9ed5280e46a49111fc9f9790026
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-27 09:45:34 -0700

    .

--- modules/users.nix
+++ modules/users.nix
@@ -17,25 +17,25 @@
     ];
   };
 
- users.users.tor = {
-   isSystemUser = false;
-   description = "User for tor/ .onion related stuff";
-   home = "/home/home/tor";
-   createHome = true;
-   shell = pkgs.nologin;
-   uid = 1001;
-   group = "tor";
-   umask = "0077";
- };
+  users.users.tor = {
+    isSystemUser = false;
+    description = "User for tor/ .onion related stuff";
+    home = "/home/home/tor";
+    createHome = true;
+    shell = pkgs.bashInteractive; # Changed from nologin to allow login
+    uid = 1001;
+    group = "tor";
+  };
 
- users.groups.tor.gid = 1001;
+  users.groups.tor.gid = 1001;
 
- systemd.tmpfiles.rules = [
-   "d /tmp/tmp 0700 tor tor - -"
-   "d /dev/shm/shm 0700 tor tor - -"
- ];
+  systemd.tmpfiles.rules = [
+    "d /tmp/tmp 0700 tor tor - -"
+    "d /dev/shm/shm 0700 tor tor - -"
+    "f /home/home/tor/.bashrc 0600 tor tor - umask 0077"
+  ];
 
- environment.etc."firejail/tor.profile".text = ''
+  environment.etc."firejail/tor.profile".text = ''
    whitelist /home/home/tor
    whitelist /tmp/tmp
    whitelist /dev/shm/shm
