26bdd0c70bf86360536c690efe327cfcbe77f169
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-29 20:47:40 -0700

    LARGE CHANGE - ADD CHAOTIC NYX AND CHANGE TO CACHY KERNEL - ON GEN 61 AS OF COMMIT

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
