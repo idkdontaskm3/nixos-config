11b516f8e76cda0d81b49054396b96ee83dfeb23
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-29 21:25:30 -0700

    .

--- modules/users.nix
+++ modules/users.nix
@@ -16,37 +16,4 @@
 
     ];
   };
-
-  users.users.toruser = {
-    isNormalUser = true;
-    description = "User for tor/ .onion related stuff";
-    home = "/home/home/toruser";
-    createHome = true;
-    shell = pkgs.bashInteractive;
-    uid = 1001;
-    group = "toruser";
-  };
-
-  users.groups.toruser.gid = 1001;
-
-  systemd.tmpfiles.rules = [
-    "d /tmp/tmp 0700 toruse toruser - -"
-    "d /dev/shm/shm 0700 toruser toruser - -"
-    "f /home/home/tor/.bashrc 0600 toruser toruser - umask 0077"
-  ];
-
-  environment.etc."firejail/toruser.profile".text = ''
-   whitelist /home/home/toruser
-   whitelist /home/home/toruser/.cache
-   whitelist /tmp/tmp
-   whitelist /dev/shm/shm
-   whitelist /etc/systemd/resolved.conf
-   blacklist /home
-   blacklist /tmp
-   blacklist /dev/shm
-   blacklist /root
-   read-only /
-   dbus-system none
-   dbus-user none
- '';
 }
