ee4bb69c4518e052952bbe664e54f6760cc4033d
idkdontaskm3 <idkdontaskm3@proton.me>
2026-08-30 14:38:02 -0700

    .

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    noctalia.url = "github:noctalia-dev/noctalia";
+    noctalia.url = "github:noctalia-dev/noctalia?rev=62bbfa1d229bf0351e8ac18d9bdfaeb5fb00fa9e;
   };
   outputs =
     inputs@{
@@ -22,25 +22,25 @@
       pkgs = nixpkgs.legacyPackages.${system}; 
     in
     {
-      nixosConfigurations = {
-        craptop = lib.nixosSystem {
-          inherit system;
-          modules = [ 
-            ./configuration.nix
-            chaotic.nixosModules.default
-          ];
-        };
-      };
-      homeConfigurations = {
-        ryan = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = { 
-            inherit inputs system;
-          };
-          modules = [ 
-            ./home/home.nix
-          ];
-        };
+    nixosConfigurations = {
+      craptop = lib.nixosSystem {
+        inherit system;
+        modules = [ 
+          ./configuration.nix
+          chaotic.nixosModules.default
+        ];
       };
     };
+    homeConfigurations = {
+      ryan = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { 
+          inherit inputs system;
+        };
+        modules = [ 
+          ./home/home.nix
+        ];
+      };
+    };
+  };
 }
