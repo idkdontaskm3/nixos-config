3eef0d1f026a464ccb46915ca7d8a85cd2e0d265
idkdontaskm3 <idkdontaskm3@proton.me>
2026-09-01 15:32:18 -0700

    .

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -8,7 +8,6 @@
   };
 
   programs.hyprland.enable = true;
-  services.displayManager.gdm.enable = true;
   services.displayManager.sddm.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
